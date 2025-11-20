a7579b93fbf748039fa3b521abb874d375a52dc1
EunjuYeom <96eunju64@gmail.com>
2025-11-20 10:19:39 +0900

    stat

--- easyhpv_stat.R
+++ easyhpv_stat.R
@@ -15,7 +15,7 @@ args <- parser$parse_args()
 Name <- args$name
 
 
-data <- read.table(sprintf("%s_result_true_filtered_annot_50M.txt", Name, Name), sep="\t", header=T)
+data <- read.table(sprintf("%s_result_true_filtered_annot_50M.txt", Name), sep="\t", header=T)
 
 integrated_read_count_df <- data %>%
   distinct(read,HPV,gene, .keep_all = TRUE) %>%
@@ -25,7 +25,7 @@ integrated_read_count_df <- data %>%
 
 write.table(
   integrated_read_count_df, 
-  file = sprintf("%s_filtered_HPV_gene_counts_50M.txt", Name, Name),
+  file = sprintf("%s_filtered_HPV_gene_counts_50M.txt", Name),
   sep = "\t",
   quote = FALSE,
   row.names = FALSE
@@ -39,9 +39,28 @@ integrated_read_count_df_2 <- data %>%
   
 write.table(
   integrated_read_count_df_2,
-  file = sprintf("%s_filtered_HPV_pos_counts_50M.txt", Name, Name),
+  file = sprintf("%s_filtered_HPV_pos_counts_50M.txt", Name),
   sep = "\t",
   quote = FALSE,
   row.names = FALSE
   )
+
+integrated_read_count_df_3 <- data %>%
+  distinct(read,HPV,gene,exon_num,type, .keep_all = TRUE) %>%
+  group_by(gene,exon_num,type, Position, HPV) %>% 
+  summarise(integrated_read_3 = n(), .groups = "drop") %>%
+  arrange(desc(integrated_read_3))
+  
+write.table(
+  integrated_read_count_df_3,
+  file = sprintf("%s_filtered_HPV_breakpoint_counts_50M.txt", Name),
+  sep = "\t",
+  quote = FALSE,
+  row.names = FALSE
+  )
+  
+  
+  
+  
+  
   
